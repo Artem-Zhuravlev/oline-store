bc03c6bf3b1b702701c6cca5f5c21cd2333f34e7
artem <mstrJesm@gmail.com>
2020-07-15 10:19:22 +0300

    added new sections

--- src/js/main.js
+++ src/js/main.js
@@ -11,5 +11,20 @@ $(document).ready(function() {
 				<img src="img/arr-right.png" alt="" />
 			</button>
 		`
+	});
+
+	$('.p-categories-slider').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		prevArrow: ` 
+			<button type="button" class="slick-prev">
+				<img src="img/a-left.png" alt="" />
+			</button>
+		`,
+		nextArrow: ` 
+			<button type="button" class="slick-next">
+				<img src="img/a-right.png" alt="" />
+			</button>
+		`
 	})
 })
